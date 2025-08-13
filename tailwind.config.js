f8648da8ecc6e0cd30e4baecc0af27b6f6367b7a
vaibhavmehta2503 <vaibhavmehtajp098@gmail.com>
2025-08-13 23:13:15 +0530

    Update 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
-/** @type {import('tailwindcss').Config} */
+
 export default {
+  darkMode: "class",
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
-  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        royal: "#0a192f", // Deep Royal Blue
+        gold: "#f5c518", // Elegant Gold
+        night: "#020817", // Dark navy
+      },
+      boxShadow: {
+        gold: "0 0 15px rgba(245, 197, 24, 0.5)",
+      },
+    },
   },
   plugins: [],
-}
+};
